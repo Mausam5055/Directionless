a842b83e4e1f0be0bb088092a4395ecdef6cb6c3
Mausam5055 <rikikumkar@gmail.com>
2026-06-19 20:07:15 +0530

    feat: add background audio toggle with autoplay interaction handler

--- js/directionless.ts
+++ js/directionless.ts
@@ -1,6 +1,7 @@
 // @ts-nocheck
-import { AnimationController } from './modules/animation.js';
+import { AnimationController } from './modules/animation.ts';
+import { audioController } from './modules/audio.ts';
 
 const animationController = new AnimationController();
-
 animationController.init();
+audioController.init();
